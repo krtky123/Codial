7bef474b6eb4bd4e8962ce7d61212f53b069bc45
Kartikay Goel <45131243+krtky123@users.noreply.github.com>
2021-09-16 21:28:49 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://krtky123:krtky123@codial.jdhtl.mongodb.net/Codial?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://krtky123:<password>@codial.jdhtl.mongodb.net/Codial?retryWrites=true&w=majority', {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
